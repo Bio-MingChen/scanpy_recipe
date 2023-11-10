4673a0a4a142fe88311787e8671f7388879f07f5
chenming <cm.bio@qq.com>
2023-11-10 17:18:58 +0800

    add tutorial

--- __init__.py
+++ __init__.py
@@ -1,1 +1,9 @@
-from scanpy_recipe.scanpy_recipe import *
+from scanpy_recipe.scanpy_recipe import (mark_doublet,
+                                         sc_setting,
+                                         basic_filter,
+                                         remove_batch_effect,
+                                         basic_prep,
+                                         pr_prep,
+                                         basic_paga,
+                                         run_celltypist,
+                                         celltypist_marker_plot)
--- setup.py
+++ setup.py
@@ -2,15 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='scanpy_recipe',
-    version='0.1',
+    version='0.11',
     packages=find_packages(),
     description='shortcut tools for scRNA-seq data analysis based on scanpy',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='Your Name',
+    author='chenming',
     author_email='cm.bio@qq.com',
-    url='https://github.com/yourusername/your_package_name',
+    url='https://github.com/Bio-MingChen/scanpy_recipe',
     install_requires=[
-        'some_package>=1.0',
+        'scanpy',
+        'pandas',
+        'numpy',
+        'anndata',
+        'scipy',
+        'matplotlib',
+        'celltypist',
+        'scanorama',
     ],
 )
